dc73ce3307931c035df6d2e05062c48d22ecef34
reizzao <reizao1977@gmail.com>
2025-06-12 02:37:36 -0300

    add

--- my_shell/in_shellscript/init.sh
+++ my_shell/in_shellscript/init.sh
@@ -17,7 +17,7 @@ alias home="cd ~/"
 alias mysc="cd $MYSHC" # Meu shell script do sistema
 alias dot="cd $MYDIR_DOTFILES"
 alias conf_nvim="cd ~/.config/nvim/"
-alias conf_vscode="cd ~/.config/User/"
+alias conf_vscode="cd ~/.config/Code/User"
 # alias vscode="cd ~/wdotfiles/Code/.config/User/"
 alias v="nvim" # abre o editor nvim <v de vim>
 
