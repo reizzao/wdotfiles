c2f72f9a26cb4f4f6e12c097052d55b9801633f5
reizzao <reizao1977@gmail.com>
2025-06-12 15:39:38 -0300

    Atualização automática - 2025-06-12 15:39:38

--- my_shell/in_shellscript/aliases.mod.sh
+++ my_shell/in_shellscript/aliases.mod.sh
@@ -5,12 +5,15 @@ alias rr="source ~/.$RC"
 alias home="cd ~/"
 alias mysc="cd $MYSHC" # Meu shell script do sistema
 
-# editores idea
-alias codium='flatpak run com.vscodium.codium'
+# use diretorios de configuracoes
 alias config_flatpaks='cd ~/.var/app/'
-alias config_origem_codium='cd ~/.var/app/com.vscodium.codium/config/VSCodium/User'
-alias config_codium_origin='cd ~/wdotfiles/codium/.var/app/com.vscodium.codium/config/VSCodium/User'
-alias config_codium='cd ~/wdotfiles/codium/'
+alias config_codium='cd ~/wdotfiles/codium/.var/app/com.vscodium.codium/config/VSCodium/User'
+
+# origens diretorios de configuracoes
+alias config_origin_codium='cd ~/.var/app/com.vscodium.codium/config/VSCodium/User'
+
+# open
+alias codium='flatpak run com.vscodium.codium' # use `codium .` para abrir o editor no local. 
 
 # my folders dev
 alias wdot="cd $MYDIR_DOTFILES"
