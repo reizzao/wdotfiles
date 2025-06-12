bed4f42e4f3b9db2d8614ef0035a0bb9293ffde9
reizzao <reizao1977@gmail.com>
2025-06-12 15:30:22 -0300

    ok

--- codium/.var/app/com.vscodium.codium/config/VSCodium/User/zzz.js
+++ codium/.var/app/com.vscodium.codium/config/VSCodium/User/zzz.js
@@ -1,3 +1,18 @@
+class Foo {
+
+  constructor(obj) {
+    this.obj.c1 = obj.c1
+    this.obj.c2 = obj.c2
+  }
+
+  acao() {
+    return "Ola acao 1"
+  }
+}
+
+const i1 = new Foo({ c1: 10, c2: 20 })
+console.log(i1)
+
 console.log("Ola Mundo Settings codium --- 1 ---")
 /* console.log("Ola Mundo Settings codium --- 1 ---")
 console.log("Ola Mundo Settings codium --- 1 ---")
