502b29bec6f88511396cd931ba0fc17aa2fb608b
reizzao <reizao1977@gmail.com>
2025-06-12 11:15:53 -0300

    feat: [mysc] add seapracao de modulos aliases do init ok

---
+++ my_shell/in_shellscript/aliases.mod.sh
@@ -0,0 +1,21 @@
+# aliases
+
+# ALIAS
+alias rr="source ~/.$RC"
+alias home="cd ~/"
+alias mysc="cd $MYSHC" # Meu shell script do sistema
+
+# my folders dev
+alias wdot="cd $MYDIR_DOTFILES"
+alias wdev="cd ~/wdev"
+alias www="cd ~/www"
+alias wbackups="cd ~/wbackups_no_git"
+alias drive_g="cd ~/wdrive_google"
+
+alias conf_nvim="cd ~/.config/nvim/"
+alias conf_vscode="cd ~/.config/Code/User"
+# alias vscode="cd ~/wdotfiles/Code/.config/User/"
+alias v="nvim" # abre o editor nvim <v de vim>
+
+# utils
+alias install_fonts="fc-cache -fv ~/.fonts"
--- my_shell/in_shellscript/init.sh
+++ my_shell/in_shellscript/init.sh
@@ -7,31 +7,12 @@ export MYSHC="/$HOME/wdotfiles/my_shell/in_shellscript"
 # EXPORTACOES
 # -- TRAZENDO OS MODULOS
 source "$MYSHC/system.mod.sh"
+source "$MYSHC/aliases.mod.sh"
 source "$MYSHC/github.mod.sh"
 source "$MYSHC/nvim.mod.sh"
 
 # VARS - SYSTEM
 export RC="bashrc" # controla o shell em uso no terminal.
-
-# ALIAS
-alias rr="source ~/.$RC"
-alias home="cd ~/"
-alias mysc="cd $MYSHC" # Meu shell script do sistema
-
-# my folders dev
-alias wdot="cd $MYDIR_DOTFILES"
-alias wdev="cd ~/wdev"
-alias www="cd ~/www"
-alias wbackups="cd ~/wbackups_no_git"
-alias drive_g="cd ~/wdrive_google"
-
-alias conf_nvim="cd ~/.config/nvim/"
-alias conf_vscode="cd ~/.config/Code/User"
-# alias vscode="cd ~/wdotfiles/Code/.config/User/"
-alias v="nvim" # abre o editor nvim <v de vim>
-
-# utils
-alias install_fonts="fc-cache -fv ~/.fonts"
 
 # CONFIGS
 # CLAUSULA stty ADICIONAR ESTA LINHA SOMENTE NO .BASHRC, PARA ACEITAR ATALHOS MAPEADOS VIM :
