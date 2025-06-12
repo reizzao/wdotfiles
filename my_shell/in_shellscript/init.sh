7bd9a07b063df9431a9025521d91861d44a0d091
reizzao <reizao1977@gmail.com>
2025-06-12 09:48:29 -0300

    feat: [my_shell] add aliases my folders dev

--- my_shell/in_shellscript/init.sh
+++ my_shell/in_shellscript/init.sh
@@ -15,7 +15,14 @@ export RC="bashrc" # controla o shell em uso no terminal.
 alias rr="source ~/.$RC"
 alias home="cd ~/"
 alias mysc="cd $MYSHC" # Meu shell script do sistema
-alias dot="cd $MYDIR_DOTFILES"
+
+# my folders dev
+alias wdot="cd $MYDIR_DOTFILES"
+alias wdev="cd ~/wdev"
+alias www="cd ~/www"
+alias wbackups="cd ~/wbackups_no_git"
+alias drive_g="cd ~/wdrive_google"
+
 alias conf_nvim="cd ~/.config/nvim/"
 alias conf_vscode="cd ~/.config/Code/User"
 # alias vscode="cd ~/wdotfiles/Code/.config/User/"
